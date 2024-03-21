bd366248511d982341521f3c355144225cbf9a2c
Rousseau <e2173165@cmaisonneuve.qc.ca>
2024-03-21 09:59:23 -0400

    nouvelle base de donné

--- functions.php
+++ functions.php
@@ -1,4 +1,13 @@
 <?php
     add_theme_support( 'menus');
     add_theme_support( 'custom-logo');
+
+    function cidweb_modifie_requete_principal( $query ) {
+        if ( $query->is_home() && $query->is_main_query() && ! is_admin() ) {
+          $query->set( 'category_name', 'note4w4' );
+          $query->set( 'orderby', 'title' );
+          $query->set( 'order', 'ASC' );
+          }
+         }
+         add_action( 'pre_get_posts', 'cidweb_modifie_requete_principal' );
 ?>
